cf0a6a1a5251b369d8d50c4d6a06241d8e4cab7b
RafaelFerSilva <rfsilva@daitan.com>
2021-03-19 17:57:12 -0300

    Add tests para a rota /contacts

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -1,24 +1,8 @@
+const ContactModel =  require('../models/contact.model')
+
 module.exports = {
-    list(request, h){
-        return [
-            {
-                id: 1,
-                name: "Rafael Fernandes da Silva",
-                number: "11 9999-9999",
-                description: "Consultor de Qualidade",
-              },
-              {
-                id: 2,
-                name: "João da Silva",
-                number: "11 9999-9998",
-                description: "Aula de Inglês",
-              },
-              {
-                id: 3,
-                name: "Maria da Silva",
-                number: "11 9999-9997",
-                description: "Aula de Francês",
-              },
-        ]
+    async list(request, h){
+      const contacts = await ContactModel.find().exec()
+      return contacts
     }
 }
---
+++ models/contact.model.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema;
+
+const contactSchema = new Schema({
+    name: String,
+    number: String,
+    description: String
+})
+
+module.exports = mongoose.model('Contact',contactSchema)
